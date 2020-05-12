10baa248e730ef15889ab81961689bbbbec8b6aa
countree103103 <717662837@qq.com>
2020-05-12 21:28:17 +0800

    *添加路由自动注册功能

--- config.py
+++ config.py
@@ -2,6 +2,8 @@
 import time
 import sys
 import os
+import importlib
+import pkgutil
 from flask import Blueprint, render_template, redirect, url_for, make_response, session, jsonify, request
 from werkzeug.routing import BaseConverter
 #from mongo import *
@@ -13,6 +15,20 @@ from flask_sqlalchemy import SQLAlchemy
 # db=Mongo(host='localhost',db_name='sk')
 # dbsql = Sqlite("./db/db.db")
 ormSqlite = SQLAlchemy()
+
+# 配置选项类
+
+
+class appConfig():
+    # app.jinja_env.auto_reload = True
+    TEMPLATES_AUTO_RELOAD = True
+    SECRET_KEY = "52013140"
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
+    # DB的连接词
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db/data.db'
+    # 默认True，SQLAlchemy会记录下对象的变动，可以理解成写log
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 # 自定义正则转换器
 
@@ -30,3 +46,15 @@ def autoEncoding():
     if(sys.version_info.major == 2):
         reload(sys)
         sys.setdefaultencoding('utf-8')
+
+
+def autoRegisterBlueprint(app, package, prefix=""):
+    print("Starts auto registering Blueprints...")
+    for _, name, _ in pkgutil.iter_modules(package.__path__):
+        module = importlib.import_module("." + name, package=prefix)
+        for item in dir(module):
+            item = getattr(module, item)
+            if isinstance(item, Blueprint):
+                app.register_blueprint(item)
+                print(f"{name} registered..")
+    print('-' * 20)
--- luyou.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from config import *
-from Views import blogAjax, blogMain, blogAuth, blogTest
-
-autoEncoding()
-
-if __name__ == "__main__":
-    print(url_for('gallery'))
